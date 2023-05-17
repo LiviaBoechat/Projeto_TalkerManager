3f8e4f3091b231d780ab12da03928f557bced7c9
LiviaBoechat <limaboechatti@gmail.com>
2023-05-17 18:15:25 -0300

    req. 04

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const generateToken = require('../utils/generateToken');
 
+const emailValidation = require('../middlewares/emailValidation');
+const passwordValidation = require('../middlewares/passwordValidation');
+
+
 const loginRouter = express.Router();
 
-loginRouter.post('/login', async (req, res) => {
-    const { email, password }= req.body;
+loginRouter.post('/login', emailValidation, passwordValidation, async (req, res) => {
     const token = generateToken();
     res.status(200).json({ token });
     
