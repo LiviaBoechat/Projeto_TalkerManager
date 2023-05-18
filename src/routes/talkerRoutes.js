8140e6f0b89749401fe8007e591afb4358f2250e
LiviaBoechat <limaboechatti@gmail.com>
2023-05-18 17:10:30 -0300

    req. 08 ok

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const { readTalkerList, readTalker, writeTalker, deleteTalker } = require('../utils/fsUtils');
+const { readTalkerList, readTalkerById, writeTalker, 
+  deleteTalker, readTalkerByName } = require('../utils/fsUtils');
 
 const ageValidation = require('../middlewares/ageValidation');
 const nameValidaton = require('../middlewares/nameValidation');
@@ -10,6 +11,24 @@ const talkerValidation = require('../middlewares/talkerValidation');
 const tokenValidation = require('../middlewares/tokenValidation');
 
 const talkerRouter = express.Router();
+
+talkerRouter.get('/talker/search', tokenValidation, async (req, res) => {
+  try {
+      const { q } = req.query;
+      if (!q) {
+        const talkerList = await readTalkerList();
+        return res.status(200).send(talkerList);
+      }
+
+      const talker = await readTalkerByName(q); // retorna array c/ 1 ou + nomes
+      if (talker.length === 0) return res.status(200).send([]);
+
+      return res.status(200).send(talker);
+  } catch (error) {
+    console.log(error);
+      return res.status(200).send([]);
+  }
+});
 
 talkerRouter.get('/talker', async (req, res) => {
     console.log('cheguei aqui');
@@ -24,7 +43,7 @@ talkerRouter.get('/talker', async (req, res) => {
  talkerRouter.get('/talker/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const talker = await readTalker(id);
+        const talker = await readTalkerById(id);
         return res.status(200).send(talker);
     } catch (error) {
         return res.status(404).send({ message: error.message });
@@ -65,7 +84,7 @@ talkerRouter.get('/talker', async (req, res) => {
     try {
       const { id } = req.params;
       const { name, age, talk } = req.body;
-      let talker = await readTalker(id); // já traz objeto do talker ref. ao id
+      let talker = await readTalkerById(id); // já traz objeto do talker ref. ao id
       talker = { ...talker, id: Number(id), name, age, talk };
       await writeTalker(talker);
       return res.status(200).send(talker);
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -4,12 +4,25 @@ const path = require('path');
 
 const talkerFile = path.resolve(__dirname, '../talker.json');
 
+const readTalkerByName = async (name) => {
+    const talkerData = await fs.readFile(talkerFile, 'utf-8');
+    const talkerList = JSON.parse(talkerData);
+    // console.log(talkerList);
+    const talker = talkerList.filter((eachTalker) => {
+        const search = eachTalker.name;
+        return search.includes(name);
+    });
+    console.log('aqui', talker);
+
+    return talker;
+  };
+
 const readTalkerList = async () => {
     const talkerData = await fs.readFile(talkerFile, 'utf-8');
     return JSON.parse(talkerData);
 };
 
-const readTalker = async (id) => {
+const readTalkerById = async (id) => {
     const talkerData = await fs.readFile(talkerFile, 'utf-8');
     const talkerList = JSON.parse(talkerData);
     const talker = talkerList.find((eachTalker) => eachTalker.id === Number(id));
@@ -28,4 +41,4 @@ const deleteTalker = async (talkerList) => {
     await fs.writeFile(talkerFile, JSON.stringify(talkerList));
 };
 
-module.exports = { readTalkerList, readTalker, writeTalker, deleteTalker };
+module.exports = { readTalkerList, readTalkerById, writeTalker, deleteTalker, readTalkerByName };
